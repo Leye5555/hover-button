d10baeed856d88152e0a00286023a2c9e900c14d
Leye5555 <aaeon456engineer@gmail.com>
2024-08-28 14:42:23 +0100

    Readme

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// change daniel's picture to havilah's picture on click;
+// // change daniel's picture to havilah's picture on click;
 
-let daniel = "./assets/daniel.jpg";
-let havilah = "./assets/havilah.jpg";
-const sample = document.getElementById("sample");
+// let daniel = "./assets/daniel.jpg";
+// let havilah = "./assets/havilah.jpg";
+// const sample = document.getElementById("sample");
 
-const img = document.createElement("img");
-img.src = daniel;
-img.alt = "daniel";
-img.width = 300;
-img.height = 400;
-img.addEventListener("click", changeImage);
-sample.appendChild(img);
-console.log(daniel);
+// const img = document.createElement("img");
+// img.src = daniel;
+// img.alt = "daniel";
+// img.width = 300;
+// img.height = 400;
+// img.addEventListener("click", changeImage);
+// sample.appendChild(img);
+// console.log(daniel);
 
-function changeImage(event) {
-  const currentImage = event.target;
-  const src = currentImage.getAttribute("src");
+// function changeImage(event) {
+//   const currentImage = event.target;
+//   const src = currentImage.getAttribute("src");
 
-  if (daniel === src) {
-    currentImage.src = havilah;
-  } else {
-    currentImage.src = daniel;
-  }
-}
+//   if (daniel === src) {
+//     currentImage.src = havilah;
+//   } else {
+//     currentImage.src = daniel;
+//   }
+// }
